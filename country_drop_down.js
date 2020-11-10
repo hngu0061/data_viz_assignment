f66bf39ce8482efd2c7dd3d20e0008d0387e6777
hngu0061 <hngu0061@student.monash.edu>
2020-11-11 01:34:29 +1100

    Update data and scatter plot

--- country_drop_down.js
+++ country_drop_down.js
@@ -1,64 +1,63 @@
-//Initialize with the list of symbols
-let names = [
-  "United States",
-  "Australia",
-  "United Kingdom",
-  "Vietnam",
-  "South Africa",
-];
+d3.csv("data/country.csv", function (data) {
+  let names = []
 
-//Find the input search box
-let search = document.getElementById("searchCountry");
-
-//Find every item inside the dropdown
-let items = document.getElementsByClassName("dropdown-item");
-function buildDropDown(values) {
-  let contents = [];
-  for (let name of values) {
-    contents.push(
-      '<input type="button" class="dropdown-item" type="button" value="' +
-        name +
-        '"/>'
-    );
+  for (var i = 0; i < data.length; i++) {
+    names.push(data[i].x)
   }
-  $("#menuItems").append(contents.join(""));
 
-  //Hide the row that shows no items were found
-  $("#empty").hide();
-}
+  //Find the input search box
+  let search = document.getElementById("searchCountry")
 
-//Capture the event when user types into the search box
-window.addEventListener("input", function () {
-  filter(search.value.trim().toLowerCase());
-});
+  //Find every item inside the dropdown
+  let items = document.getElementsByClassName("dropdown-item")
+  function buildDropDown(values) {
+    let contents = []
+    for (let name of values) {
+      contents.push(
+        '<input type="button" class="dropdown-item" type="button" value="' +
+          name +
+          '"/>'
+      )
+    }
+    $("#menuItems").append(contents.join(""))
 
-//For every word entered by the user, check if the symbol starts with that word
-//If it does show the symbol, else hide it
-function filter(word) {
-  let length = items.length;
-  let collection = [];
-  let hidden = 0;
-  for (let i = 0; i < length; i++) {
-    if (items[i].value.toLowerCase().startsWith(word)) {
-      $(items[i]).show();
+    //Hide the row that shows no items were found
+    $("#empty").hide()
+  }
+
+  //Capture the event when user types into the search box
+  window.addEventListener("input", function () {
+    filter(search.value.trim().toLowerCase())
+  })
+
+  //For every word entered by the user, check if the symbol starts with that word
+  //If it does show the symbol, else hide it
+  function filter(word) {
+    let length = items.length
+    let collection = []
+    let hidden = 0
+    for (let i = 0; i < length; i++) {
+      if (items[i].value.toLowerCase().startsWith(word)) {
+        $(items[i]).show()
+      } else {
+        $(items[i]).hide()
+        hidden++
+      }
+    }
+
+    //If all items are hidden, show the empty view
+    if (hidden === length) {
+      $("#empty").show()
     } else {
-      $(items[i]).hide();
-      hidden++;
+      $("#empty").hide()
     }
   }
 
-  //If all items are hidden, show the empty view
-  if (hidden === length) {
-    $("#empty").show();
-  } else {
-    $("#empty").hide();
-  }
-}
+  //If the user clicks on any item, set the title of the button as the text of the item
+  $("#menuItems").on("click", ".dropdown-item", function () {
+    $("#dropdown_countries").text($(this)[0].value)
+    $("#dropdown_countries").dropdown("toggle")
+  })
 
-//If the user clicks on any item, set the title of the button as the text of the item
-$("#menuItems").on("click", ".dropdown-item", function () {
-  $("#dropdown_countries").text($(this)[0].value);
-  $("#dropdown_countries").dropdown("toggle");
-});
-
-buildDropDown(names);
+  buildDropDown(names)
+})
--- scatter_plot.js
+++ scatter_plot.js
@@ -5,7 +5,7 @@
 function draw_scatter_plot(data, option = "price") {
   let margin = { top: 10, right: 100, bottom: 30, left: 30 },
     width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    height = 400 - margin.top - margin.bottom
 
   // append the svg object to the body of the page
   let svg = d3
@@ -14,146 +14,99 @@ function draw_scatter_plot(data, option = "price") {
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
   // Read csv data
   var price_range = d3.extent(data, function (data) {
-    return Number(data.price);
-  });
+    return Number(data.price)
+  })
 
   var point_range = d3.extent(data, function (data) {
-    return Number(data.points);
-  });
+    return Number(data.points)
+  })
 
-  var vintage_year_range = [0, 2000];
-  //   var vintage_year_range = d3.extent(data, function (data) {
-  //     return Number(data.vintage_year);
-  //   });
+  var vintage_year_range = d3.extent(data, function (data) {
+    return Number(data.vintage_year)
+  })
 
   var domain_range =
     option === "price"
       ? price_range
       : option === "year"
       ? vintage_year_range
-      : [0, 100];
+      : vintage_year_range
 
   // Add X axis --> can be price/vintage year
-  var x = d3.scaleLinear().domain(domain_range).range([0, width]);
+  var x = d3.scaleLinear().domain(domain_range).range([0, width])
   svg
     .append("g")
     .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
+    .call(d3.axisBottom(x))
 
   // Add Y axis
-  var y = d3.scaleLinear().domain(point_range).range([height, 0]);
-  svg.append("g").call(d3.axisLeft(y));
+  var y = d3.scaleLinear().domain(point_range).range([height, 0])
+  svg.append("g").attr("class", "yAxis").call(d3.axisLeft(y))
+
+  var myColor = d3
+    .scaleSequential()
+    .domain(price_range)
+    .interpolator(d3.interpolateRdYlBu)
+
+  // Initialize dots with group a
+  var dot = svg
+    .selectAll("circle")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cx", function (d) {
+      if (option === "price") {
+        return x(d.price)
+      } else {
+        return x(d.vintage_year)
+      }
+    })
+    .attr("cy", function (d) {
+      return y(d.points)
+    })
+    .attr("r", 3)
+    .style("fill", function (d) {
+      if (option === "price_year") {
+        return myColor(d.price)
+      } else {
+        return "#69b3a2"
+      }
+    })
 }
 
-// //Read the data
-// d3.csv(
-//   "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_connectedscatter.csv",
-//   function (data) {
-//     // List of groups (here I have one group per column)
-//     // var allGroup = ["Price", "Vintage Year", "Price and Vintage Year"];
-//     var allGroup = ["valueA", "valueB", "valueC"];
-
-//     // add the options to the button
-//     d3.select("#selectButton")
-//       .selectAll("myOptions")
-//       .data(allGroup)
-//       .enter()
-//       .append("option")
-//       .text(function (d) {
-//         return d;
-//       }) // text showed in the menu
-//       .attr("value", function (d) {
-//         return d;
-//       }); // corresponding value returned by the button
-
-//     // Add X axis --> it is a date format
-//     var x = d3.scaleLinear().domain([0, 10]).range([0, width]);
-//     svg
-//       .append("g")
-//       .attr("transform", "translate(0," + height + ")")
-//       .call(d3.axisBottom(x));
-
-//     // Add Y axis
-//     var y = d3.scaleLinear().domain([0, 20]).range([height, 0]);
-//     svg.append("g").call(d3.axisLeft(y));
-
-//     // Initialize dots with group a
-//     var dot = svg
-//       .selectAll("circle")
-//       .data(data)
-//       .enter()
-//       .append("circle")
-//       .attr("cx", function (d) {
-//         return x(+d.time);
-//       })
-//       .attr("cy", function (d) {
-//         return y(+d.valueA);
-//       })
-//       .attr("r", 7)
-//       .style("fill", "#69b3a2");
-
-//     // A function that update the chart
-//     function update(selectedGroup) {
-//       // Create new data with the selection?
-//       var dataFilter = data.map(function (d) {
-//         return { time: d.time, value: d[selectedGroup] };
-//       });
-
-//       dot
-//         .data(dataFilter)
-//         .transition()
-//         .duration(1000)
-//         .attr("cx", function (d) {
-//           return x(+d.time);
-//         })
-//         .attr("cy", function (d) {
-//           return y(+d.value);
-//         });
-//     }
-
-//     // When the button is changed, run the updateChart function
-//     d3.select("#selectButton").on("change", function (d) {
-//       // recover the option that has been chosen
-//       var selectedOption = d3.select(this).property("value");
-//       // run the updateChart function with this selected option
-//       update(selectedOption);
-//     });
-//   }
-// );
-
 // Load data file
-d3.csv("data/winemag-data-130k-v2.csv", function (data) {
-  console.log(data.length);
+d3.csv("data/data_viz.csv", function (data) {
+  console.log(data.length)
 
   // Draw default scatter plot
-  draw_scatter_plot(data);
+  draw_scatter_plot(data)
 
   // Track change of country
   $("#menuItems").on("click", ".dropdown-item", function () {
-    console.log("Change country");
-    draw_scatter_plot(data);
-  });
+    console.log("Change country")
+    draw_scatter_plot(data)
+  })
 
   // Track change in radio button
   function change_factor() {
-    var form = document.getElementById("scatter_plot_factor");
-    var form_val;
+    var form = document.getElementById("scatter_plot_factor")
+    var form_val
     for (var i = 0; i < form.length; i++) {
       if (form[i].checked) {
-        form_val = form[i].value;
+        form_val = form[i].value
       }
     }
 
-    var selected_country = document.querySelector("#dropdown_countries");
+    var selected_country = document.querySelector("#dropdown_countries")
     //   console.log(selected_country.textContent);
 
-    draw_scatter_plot(data, form_val);
+    draw_scatter_plot(data, form_val)
   }
 
-  var factor_option = d3.select("#scatter_plot_factor");
-  factor_option.on("change", change_factor);
-});
+  var factor_option = d3.select("#scatter_plot_factor")
+  factor_option.on("change", change_factor)
+})
